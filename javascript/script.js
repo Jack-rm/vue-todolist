792a47507e123b7e2c1e06154db4890c13a91fb3
Jack-rm <jackcnsr94@gmail.com>
2021-09-16 15:43:51 +0200

    Initialization of todolist vuejs exercise

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+/**
+ * Descrizione:
+ * 
+ * Rifacciamo l'esercizio della toDoList come fatto assieme in classe, suddividendo i nostri -milestone- principalmente in questi step:
+ * - Stampare in pagina un item per ogni elemento contenuto in un array
+ * - Ogni item ha una icona "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+ * - Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante,
+ *   il testo digitato sarà aggiunto alla lista
+*/
+
+let root = new Vue (
+    {
+        el: "#root",
+
+        data: {
+
+            toDoList: [ "pane", "latte", "uova", "formaggio", "insalata", "pomodori", "melanzane", "zucchine", "carciofi", "mele", "pere", "cocomero", "melone", "uva", "vino", "carne", "pesce", "funghi", "fagioli", "ceci"],
+        },
+
+        methods: {},
+    }
+);
+
